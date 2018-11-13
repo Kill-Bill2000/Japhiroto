ec57d1a12cd472da7a8761a020e5011a15d1e0b3
Robin <rob.konten@outlook.de>
2018-11-13 18:08:12 +0100

    Konstruktor hinzugefügt, verbindungAufbauen hinzugefügt

--- src/japhiroto/DB_Verbindung.java
+++ src/japhiroto/DB_Verbindung.java
@@ -5,10 +5,34 @@
  */
 package japhiroto;
 
+import java.sql.*;
+
 /**
  *
  * @author robin
  */
 public class DB_Verbindung {
+    private Connection con;
+    private Statement stmt;
+    private ResultSet rs;
+    private String url, dbHost, dbName, dbUser, dbPass;
+    private String uri = "jdbc:mysql://sql7.freemysqlhosting.net/sql7265228";
+    
+    
+    public DB_Verbindung(String host, String name, String user, String pass){
+        this.dbHost = host;
+        this.dbName = name;
+        this.dbUser = user;
+        this.dbPass = pass;
+    }
+    
+    public boolean verbindungAufbauen() throws SQLException{
+        //baut eine Verbindung zur Datenbank mit Host-Adresse, Benutzername und Passwort der DB auf
+        //gibt 'true' zurück, wenn die Verbindung nach 5 Sekunden noch nicht geschlossen und valide ist
+        
+        con = DriverManager.getConnection("jdbc:mysql://" + dbHost + "/" + dbName, dbUser, dbPass);
+        return con.isValid(5);
+    }
+
     
 }
