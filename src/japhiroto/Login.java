1c8e39bb43e656ed2c59d2618ceb310fd4f8c9fa
Robin <rob.konten@outlook.de>
2018-12-05 18:59:33 +0100

    Login: unbenutzte Imports entfernt

--- src/japhiroto/Login.java
+++ src/japhiroto/Login.java
@@ -5,9 +5,7 @@
  */
 package japhiroto;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
