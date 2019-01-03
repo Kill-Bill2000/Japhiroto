d463a30c199c5405d52b9519550c9d6c707d8e45
janbo <janbo@JansGamingDesktop.fritz.box>
2019-01-03 16:20:00 +0100

    Verbinden der Klassen zu Schichten

--- src/japhiroto/Kasse_Verwaltung.java
+++ src/japhiroto/Kasse_Verwaltung.java
@@ -5,10 +5,17 @@
  */
 package japhiroto;
 
+    public DB_Verbindung dieDB_Verbindung;
+
 /**
  *
  * @author Jan
  */
 public class Kasse_Verwaltung {
+    
+    public Artikel artikelSuchen(){
+        
+    }
+    
     
 }
