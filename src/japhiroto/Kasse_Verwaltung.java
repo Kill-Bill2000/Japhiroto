a6c39c8872aad01c2d55f9c918f7ef8a631e2a1d
janbo <janbo@JansGamingDesktop.fritz.box>
2019-01-07 00:44:24 +0100

    kleine aenderung

--- src/japhiroto/Kasse_Verwaltung.java
+++ src/japhiroto/Kasse_Verwaltung.java
@@ -95,7 +95,7 @@ public class Kasse_Verwaltung {
             if(anzahlBuchstaben>16){
                 String teil1 = name.substring(0, 14);
                 String teil2 = name.substring(14);
-                anzahlLeerzeichen = 28 - teil2.length();
+                anzahlLeerzeichen = 27 - teil2.length();
                 for (int j=0; j<=anzahlLeerzeichen; j++){
                     leerzeichen = leerzeichen +" ";
                 }
