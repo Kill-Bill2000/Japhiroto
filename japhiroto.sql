0052faafa7129ac805e228fca11bf7274b95b86f
T0masHr <44921318+T0masHr@users.noreply.github.com>
2019-01-07 17:32:46 +0100

    uniting japhiroto.sql and sql.sql, japhiroto stays.sql as the only db file

--- japhiroto.sql
+++ japhiroto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Erstellungszeit: 06. Jan 2019 um 19:35
--- Server-Version: 10.1.31-MariaDB
--- PHP-Version: 7.2.3
+-- Host: 127.0.0.1
+-- Erstellungszeit: 05. Jan 2019 um 19:29
+-- Server-Version: 10.1.37-MariaDB
+-- PHP-Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,11 +21,13 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `japhiroto`
 --
+CREATE DATABASE IF NOT EXISTS `japhiroto` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `japhiroto`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
+-- Tabellenstruktur für Tabelle `accounts`
 --
 
 CREATE TABLE `accounts` (
@@ -37,7 +39,7 @@ CREATE TABLE `accounts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
 --
--- Dumping data for table `accounts`
+-- Daten für Tabelle `accounts`
 --
 
 INSERT INTO `accounts` (`accountId`, `mitarbeiterId`, `benutzername`, `passwort`, `rolle`) VALUES
@@ -48,50 +50,46 @@ INSERT INTO `accounts` (`accountId`, `mitarbeiterId`, `benutzername`, `passwort`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
---
-
-CREATE TABLE `accounts` (
-  `accountId` int(16) NOT NULL,
-  `mitarbeiterId` int(16) NOT NULL,
-  `benutzername` varchar(32) COLLATE latin1_german1_ci NOT NULL,
-  `passwort` varchar(32) COLLATE latin1_german1_ci NOT NULL,
-  `rolle` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
-
---
--- Dumping data for table `accounts`
---
-
-INSERT INTO `accounts` (`accountId`, `mitarbeiterId`, `benutzername`, `passwort`, `rolle`) VALUES
-(1, 1, 'm.mustermann.leiter', 'muster123', 0),
-(2, 1, 'm.mustermann.kasse', 'muster123', 1),
-(3, 1, 'm.mustermann.lager', 'muster123', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `artikel`
+-- Tabellenstruktur für Tabelle `artikel`
 --
 
 CREATE TABLE `artikel` (
-  `artikelNr` int(16) NOT NULL,
-  `artikelNummer` char(32) COLLATE utf8_german2_ci NOT NULL,
-  `bezeichnung` varchar(64) COLLATE utf8_german2_ci NOT NULL,
-  `preis` double(9,2) NOT NULL,
-  `bestand` int(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+  `artikelNummer` char(32) NOT NULL,
+  `artikelName` char(64) NOT NULL,
+  `verkaufPreis` double NOT NULL,
+  `bestand` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `artikel`
+-- Daten für Tabelle `artikel`
 --
 
-INSERT INTO `artikel` (`artikelNr`, `artikelNummer`, `bezeichnung`, `preis`, `bestand`) VALUES
-(1, '1', 'Testartikel1', 9.99, 10),
-(2, '2', 'Testartikel2', 4.49, 1),
-(3, '3', 'Testartikel3', 29.99, 59),
-(4, '9006900014773', 'Pfanner Roter Tee Zitrone Granatapfel 2L', 1.99, 10),
-(5, '4006613002932', 'Hello Kitty Adventskalender', 4.99, 25);
+INSERT INTO `artikel` (`artikelNummer`, `artikelName`, `verkaufPreis`, `bestand`) VALUES
+('9006900014773', 'Pfanner Roter Tee Zitrone Granatapfel 2L', 1.99, 10);
+INSERT INTO `artikel` (`artikelNummer`, `artikelName`, `verkaufPreis`, `bestand`) VALUES
+('457842','Coca Cola 1L',0.65,25);
+INSERT INTO `artikel` (`artikelNummer`, `artikelName`, `verkaufPreis`, `bestand`) VALUES
+('789456','Jack Daniels Whiskey 0,75L',19.95,8);
+INSERT INTO `artikel` (`artikelNummer`, `artikelName`, `verkaufPreis`, `bestand`) VALUES
+('531874','Nutella 2,5KG',14.99,100);
+
+-- --------------------------------------------------------
+--
+-- Tabellenstruktur für Tabelle `umsatz`
+--
+
+CREATE TABLE `umsatz` (
+  `umsatzNr` int NOT NULL,
+  `zeitstempel` timestamp NOT NULL,
+  `umsatz` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `artikel`
+--
+
+INSERT INTO `umsatz` (`umsatzNr`, `zeitstempel`, `umsatz`) VALUES
+('0','2019-01-07 01:05:00','1.00');
 
 -- --------------------------------------------------------
 
@@ -100,18 +98,10 @@ INSERT INTO `artikel` (`artikelNr`, `artikelNummer`, `bezeichnung`, `preis`, `be
 --
 
 CREATE TABLE `bestellteartikel` (
-  `bestellID` char(32) NOT NULL,
-  `artikelID` char(32) NOT NULL,
+  `bestellID` int(10) NOT NULL,
+  `artikelID` int(10) NOT NULL,
   `anzahl` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `bestellteartikel`
---
-
-INSERT INTO `bestellteartikel` (`bestellID`, `artikelID`, `anzahl`) VALUES
-('123456789', '4006613002932', 2),
-('123456789', '9006900014773', 2);
 
 -- --------------------------------------------------------
 
@@ -125,17 +115,10 @@ CREATE TABLE `bestellung` (
   `erledigt` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Daten für Tabelle `bestellung`
---
-
-INSERT INTO `bestellung` (`bestellNummer`, `lieferant`, `erledigt`) VALUES
-('123456789', 'Obst und Gemüse GmbH', 0);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mitarbeiter`
+-- Tabellenstruktur für Tabelle `mitarbeiter`
 --
 
 CREATE TABLE `mitarbeiter` (
@@ -151,7 +134,7 @@ CREATE TABLE `mitarbeiter` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
 --
--- Dumping data for table `mitarbeiter`
+-- Daten für Tabelle `mitarbeiter`
 --
 
 INSERT INTO `mitarbeiter` (`mitarbeiterId`, `anrede`, `vorname`, `nachname`, `strasse`, `hausNr`, `plz`, `ort`, `stundenLohn`) VALUES
@@ -160,32 +143,7 @@ INSERT INTO `mitarbeiter` (`mitarbeiterId`, `anrede`, `vorname`, `nachname`, `st
 (3, 'Dive', 'Maxia*innen', 'Musterfrau*innen', 'Mustergasse*innen', 61, 7531459, 'ZweiteMusterstadt*innen', 0.01);
 
 --
--- Table structure for table `mitarbeiter`
---
-
-CREATE TABLE `mitarbeiter` (
-  `mitarbeiterId` int(16) NOT NULL,
-  `anrede` varchar(4) COLLATE latin1_german1_ci DEFAULT NULL,
-  `vorname` varchar(32) COLLATE latin1_german1_ci NOT NULL,
-  `nachname` varchar(32) COLLATE latin1_german1_ci NOT NULL,
-  `strasse` varchar(32) COLLATE latin1_german1_ci DEFAULT NULL,
-  `hausNr` int(8) DEFAULT NULL,
-  `plz` int(5) DEFAULT NULL,
-  `ort` varchar(61) COLLATE latin1_german1_ci DEFAULT NULL,
-  `stundenLohn` double(9,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
-
---
--- Dumping data for table `mitarbeiter`
---
-
-INSERT INTO `mitarbeiter` (`mitarbeiterId`, `anrede`, `vorname`, `nachname`, `strasse`, `hausNr`, `plz`, `ort`, `stundenLohn`) VALUES
-(1, 'Herr', 'Max', 'Mustermann', 'Musterstrasse', 1, 12345, 'Musterstadt', 8.50),
-(2, 'Frau', 'Maxia', 'Musterfrau', 'Mustergasse', 2, 67890, 'ZweiteMusterstadt', 5.80),
-(3, 'Dive', 'Maxia*innen', 'Musterfrau*innen', 'Mustergasse*innen', 61, 7531459, 'ZweiteMusterstadt*innen', 0.01);
-
---
--- Indexes for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
@@ -198,11 +156,7 @@ ALTER TABLE `accounts`
 -- Indizes für die Tabelle `artikel`
 --
 ALTER TABLE `artikel`
-  ADD PRIMARY KEY (`artikelNr`,`artikelNummer`);
--- Indexes for table `accounts`
---
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`accountId`);
+  ADD PRIMARY KEY (`artikelNummer`);
 
 --
 -- Indizes für die Tabelle `bestellteartikel`
@@ -226,11 +180,6 @@ ALTER TABLE `mitarbeiter`
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
--- AUTO_INCREMENT für Tabelle `artikel`
---
-ALTER TABLE `artikel`
-  MODIFY `artikelNr` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
 --
 -- AUTO_INCREMENT für Tabelle `accounts`
 --
@@ -246,3 +195,4 @@ COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
